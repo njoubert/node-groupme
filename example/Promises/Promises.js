daf56529047bff96cc10da24c81f2903a3b468eb
Niels Joubert <njoubert@gmail.com>
2013-06-11 20:50:12 -0700

    Now with an example of how to wrap these in Q promises

---
+++ example/Promises/Promises.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+var API = require('../../index').Stateless;
+var Q   = require('Q');
+
+
+if (process.argv.length < 3) {
+    console.log("Usage: node HelloWorld.js ACCESS_TOKEN");
+    process.exit(1);
+} 
+var ACCESS_TOKEN = process.argv[2];
+
+var qfunc = function() {
+    var args = Array.prototype.slice.call(arguments);
+    return Q.nfapply(this, args);
+}
+
+
+for (g in API) {
+    for (f in API[g]) {
+        API[g][f].Q = qfunc;
+    }
+}
+
+
+API.Users.me.Q(ACCESS_TOKEN)
+    .then(
+        function(da) { 
+            var id = da.id;
+            return API.Groups.index.Q(ACCESS_TOKEN); 
+        })
+    .then (
+        function(da) {
+            console.log(da);
+        });
