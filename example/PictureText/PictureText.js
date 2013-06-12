9ae1befe782a8422f8dec10c28f8267ce8b5fc89
Niels Joubert <njoubert@gmail.com>
2013-06-12 15:04:48 -0700

    an example for uploading pictures

---
+++ example/PictureText/PictureText.js
@@ -0,0 +1,17 @@
+var ImageService = require('../../index').ImageService;
+var fs   = require('fs');
+var path = require('path');
+
+if (process.argv.length < 4) {
+    console.log("Usage: node HelloWorld.js ACCESS_TOKEN IMAGE");
+    process.exit(1);
+} 
+var ACCESS_TOKEN = process.argv[2];
+var IMAGE_PATH   = process.argv[3];
+
+
+
+ImageService.post(
+    fs.createReadStream(path.join(__dirname, IMAGE_PATH)), 
+        function(err,ret) {});
+
