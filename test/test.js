63afede0b95675f26aace50495c3628e21c932b9
Niels Joubert <njoubert@gmail.com>
2013-06-14 20:36:38 -0700

    Adding missing timer var

--- test/test.js
+++ test/test.js
@@ -50,8 +50,7 @@ var startTimeout = function(description) {
 var testGroupName = "TestGroup" + Date.now();
 
 //Placeholders for timeouts.
-var t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13;
-
+var t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14;
 
 
 /************************************************************************
